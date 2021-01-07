3bc9d9993f80da91b7214dcdd0e9c9dcfd548d22
sirvaan <nemati.sirwaan@yahoo.com>
2021-01-06 20:33:30 -0800

    Corrected to not use table()

--- Assignment1AirPollution/complete.R
+++ Assignment1AirPollution/complete.R
@@ -1,12 +1,13 @@
 complete <- function(directory, id = 1:332) {
   file_list <- list.files(directory, full.names = T)
   airquality <- data.frame()
+  nobs <- vector()
   for (i in id) {
-    airquality <- rbind(airquality, read.csv(file_list[i]))
+    airquality <- read.csv(file_list[i])
+    good <- complete.cases(airquality)
+    airquality_subset <- airquality[good,]
+    nobs <- c(nobs,nrow(airquality_subset))
   }
-  good <- complete.cases(airquality)
-  airquality_subset <- airquality[good,]
-  complete_dat <- as.data.frame(table(airquality_subset$ID), responseName = "nobs")
-  colnames(complete_dat)[1] <- "id"
-  complete_dat
+  result <- cbind.data.frame(id,nobs)
+  result
 }
